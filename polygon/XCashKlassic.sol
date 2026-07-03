9159e917e7a2ec69bdb8bb8d7e85ef3b98133a49
minerjed <az0006t@protonmail.com>
2026-07-02 22:47:14 -0400

    Add roles for future use

--- polygon/XCashKlassic.sol
+++ polygon/XCashKlassic.sol
@@ -2,30 +2,47 @@
 // Compatible with OpenZeppelin Contracts ^5.6.0
 pragma solidity ^0.8.27;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
 /// @custom:security-contact az0006t@protonmail.com
-contract XCashKlassic is ERC20, ERC20Pausable, Ownable {
-    constructor(address initialOwner)
-        ERC20("XCash Klassic", "wXCK")
-        Ownable(initialOwner)
-    {}
+contract XCashKlassic is ERC20, ERC20Pausable, AccessControl {
+    /// @notice Authorized to mint wrapped XCK.
+    /// @dev Intended to be assigned to the bridge wallet or bridge contract.
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    function pause() public onlyOwner {
+    /// @notice Authorized to pause and unpause transfers in an emergency.
+    /// @dev Intended to be assigned to an emergency administrator.
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    constructor(address initialAdmin)
+        ERC20("XCash Klassic", "wXCK")
+    {
+        require(initialAdmin != address(0), "Admin cannot be zero address");
+        // Initially assign all roles to the admin.
+        // Roles can be separated later without redeploying the token.
+        _grantRole(DEFAULT_ADMIN_ROLE, initialAdmin);
+        _grantRole(MINTER_ROLE, initialAdmin);
+        _grantRole(PAUSER_ROLE, initialAdmin);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        require(to != address(0), "Invalid recipient");
+        require(amount > 0, "Invalid amount");
         _mint(to, amount);
     }
 
     function burn(uint256 amount) public {
+        require(amount > 0, "Invalid amount");
         _burn(msg.sender, amount);
     }
 
@@ -38,5 +55,14 @@ contract XCashKlassic is ERC20, ERC20Pausable, Ownable {
         override(ERC20, ERC20Pausable)
     {
         super._update(from, to, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
